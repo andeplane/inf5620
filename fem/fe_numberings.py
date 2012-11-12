599664b58621e71cdab50275157390973c737220
Anders Hafreager <anders@tapcat.is>
2012-11-12 11:25:36 +0100

    Added the rest of exc 8

--- fem/fe_numberings.py
+++ fem/fe_numberings.py
@@ -1,12 +1,26 @@
 from sympy import *
-from approx1D import *
-from fe_approx1D_v1 import *
 
-Omega = [0,2]
-nodes = [0,1,1.2,2]
-elements = [[0,1], [1,2], [2,3]]
+nodes_1 = [0,1,1.2,2]
+nodes_2 = [0,1,1.2,2]
 
-nodes.insert(3,1.6)
-elements.append([3,4])
-print nodes
-print elements
+elements_1 = [[0,1], [1,2], [2,3]]
+elements_2 = [[0,1], [1,2], [2,3]]
+
+# Create the inversed ordering
+nodes_2.reverse()
+[el.reverse() for el in elements_2]
+elements_2.reverse()
+
+print "Left-right ordering"
+print nodes_1
+print elements_1
+print "Right-left ordering"
+print nodes_2
+print elements_2
+
+# Insert node at the correct position. Element elements_1[2] is still correct since it points to the index only
+nodes_1.insert(3,1.6)
+elements_1.append([3,4])
+print "Fixing elements:"
+print nodes_1
+print elements_1
