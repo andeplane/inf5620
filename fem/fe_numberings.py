8f18d4d88e4e55d1962a11d1af9c1141386439ac
Anders Hafreager <anders@tapcat.is>
2012-11-12 11:41:29 +0100

    Now I think exc 8 is correct

--- fem/fe_numberings.py
+++ fem/fe_numberings.py
@@ -8,8 +8,6 @@ elements_2 = [[0,1], [1,2], [2,3]]
 
 # Create the inversed ordering
 nodes_2.reverse()
-[el.reverse() for el in elements_2]
-elements_2.reverse()
 
 print "Left-right ordering"
 print nodes_1
@@ -19,8 +17,14 @@ print nodes_2
 print elements_2
 
 # Insert node at the correct position. Element elements_1[2] is still correct since it points to the index only
+print "Fixing left-right ordered elements:"
 nodes_1.insert(3,1.6)
 elements_1.append([3,4])
-print "Fixing elements:"
 print nodes_1
 print elements_1
+
+print "Fixing right-left ordered elements:"
+nodes_2.insert(1,1.6)
+elements_2.append([3,4])
+print nodes_2
+print elements_2
