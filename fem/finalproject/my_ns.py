bf60aaf86c8d44897b0852d25315d973975f083d
Anders Hafreager <anders@tapcat.is>
2012-12-05 11:07:00 +0100

    NS works

--- fem/finalproject/my_ns.py
+++ fem/finalproject/my_ns.py
@@ -1,7 +1,8 @@
 from dolfin import *
+import numpy as np
 
 #mesh = UnitSquare(10,10)
-mesh = Rectangle(0,0,2,1,10,10,'left')
+mesh = Rectangle(0,0,2,1,20,20,'left')
 
 # Define function spaces (P2-P1)
 V = VectorFunctionSpace(mesh, "CG", 2)
@@ -10,7 +11,6 @@ Q = FunctionSpace(mesh, "CG", 1)
 # Define trial and test functions
 u = TrialFunction(V) # Trial function for u
 p = TrialFunction(Q) # Trial function for p
-phi = TrialFunction(Q) # Trial function for phi
 
 v = TestFunction(V) # Test function for u
 q = TestFunction(Q) # Test function for p
@@ -20,12 +20,11 @@ n = FacetNormal(mesh)
 
 # Set parameter values
 dt = 0.01 # Timestep
-T = 4     # Final time
-nu = 1.0 # Kinematic viscosity
+T = 40     # Final time
+nu = 0.5 # Kinematic viscosity
 rho = 1.0    # Density
-beta = 1.0   
 
-p_in = Constant(8.0) # Pressure at x=0
+p_in = Constant(1.0) # Pressure at x=0
 #p_in = Expression("sin(2*pi*t)", t=0.0)
 p_out = Constant(0.0) # Pressure at x=1
 # No slip makes sure that the velocity field is zero at the boundaries
@@ -47,41 +46,39 @@ bcp = [inflow, outflow]
 # Create functions
 u0 = Function(V)
 u1 = Function(V)
+p0 = Function(Q)
 p1 = Function(Q)
-phi1 = Function(Q)
 
 # Define coefficients
 k = Constant(dt)
 f = Constant((0, 0))
 
 # Tentative velocity step
+U = 0.5*(u0 + u)
+
 F1 = (1/k)*inner(u - u0, v)*dx \
-    + nu*inner(grad(u), grad(v))*dx \
+    + nu*inner(grad(U), grad(v))*dx \
+    - p0*div(v)*dx\
     - inner(f, v)*dx\
     + inner(grad(u0)*u0, v)*dx \
-    #- beta/rho*p1*div(v)*dx\
-    #+ p1*inner(n,v)*ds
-
+    + inner(p0*n,v)*ds\
+    
 a1 = lhs(F1)
 L1 = rhs(F1)
 
 # Calculation of phi
-a2 = inner(grad(phi), grad(q))*dx
-L2 = -(rho/k)*div(u1)*q*dx
+a2 = inner(grad(p), grad(q))*dx
+L2 = inner(grad(p0),grad(q)) *dx\
+    -(1.0/k)*div(u1)*q*dx
 
 # Velocity update
 a3 = inner(u, v)*dx
-L3 = inner(u1, v)*dx - k/rho*inner(grad(phi1), v)*dx
-
-# Pressure update
-a4 = p*q*dx
-L4 = (phi1 + beta*p1)*q*dx
+L3 = inner(u1, v)*dx - k*inner(grad(p1 - p0), v)*dx
 
 # Assemble matrices
 A1 = assemble(a1)
 A2 = assemble(a2)
 A3 = assemble(a3)
-A4 = assemble(a4)
 
 # Create files for storing solution
 ufile = File("results/velocity.pvd")
@@ -90,33 +87,23 @@ pfile = File("results/pressure.pvd")
 # Time-stepping
 t = dt
 while t < T + DOLFIN_EPS:
-	# Update pressure boundary condition
+    # Update pressure boundary condition
     p_in.t = t
 
     # Compute tentative velocity step
-    #begin("Computing tentative velocity")
     b1 = assemble(L1)
     [bc.apply(A1, b1) for bc in bcu]
     solve(A1, u1.vector(), b1, "gmres", "default")
-    #end()
-
+    
     # Pressure correction
-    #begin("Computing pressure correction")
     b2 = assemble(L2)
     [bc.apply(A2, b2) for bc in bcp]
-    solve(A2, phi1.vector(), b2, "gmres", "amg")
-    #end()
+    solve(A2, p1.vector(), b2, "gmres", "amg")
 
     # Velocity correction
-    #begin("Computing velocity correction")
     b3 = assemble(L3)
     [bc.apply(A3, b3) for bc in bcu]
     solve(A3, u1.vector(), b3, "gmres", "default")
-    #end()
-
-    b4 = assemble(L4)
-    [bc.apply(A4, b4) for bc in bcp]
-    solve(A4, p1.vector(), b4, "gmres", "amg")
 
     plot(u1, title="Velocity", rescale=True)
 
@@ -126,8 +113,14 @@ while t < T + DOLFIN_EPS:
 
     # Move to next time step
     u0.assign(u1)
+    p0.assign(p1)
     t += dt
+
+    u_vec = u1.vector()
+    u_array = u_vec.array()
     print "t =", t
+    print "max: ",u_array.max()
+
 
 # Hold plot
 interactive()
